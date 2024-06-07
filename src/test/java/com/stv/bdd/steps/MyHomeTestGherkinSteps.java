a263efb0f6cb73b8dd573e09be86fc7369f7d9e2
Lara <kulikova2.22larisa@gmail.com>
2024-06-07 22:14:31 +0300

    Ready

--- src/test/java/com/stv/bdd/steps/MyHomeTestGherkinSteps.java
+++ src/test/java/com/stv/bdd/steps/MyHomeTestGherkinSteps.java
@@ -3,26 +3,13 @@ package com.stv.bdd.steps;
 import com.stv.factory.factorypages.MainFactoryPage;
 import com.stv.factory.factorytests.BasicFactoryTest;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import java.time.Duration;
-
-import static com.stv.framework.core.lib.ParaBankPageURLs.START_URL;
 
 public class MyHomeTestGherkinSteps extends BasicFactoryTest {
     MainFactoryPage mainFactoryPage = new MainFactoryPage();
-    WebDriver driver = getDriver();
-
-    @Given("the user has opened the login page")
-    public void theUserHasOpenedTheLoginPage() {
-        driver.get(START_URL);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.manage().window().maximize();
-        System.out.println("the user has opened the login page- ready");
-    }
 
     @When("the user does not enter data in the username and password fields")
     public void theUserDoesNotEnterDataInTheUsernameAndPasswordFields() {
